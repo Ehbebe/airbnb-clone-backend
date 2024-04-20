780deb3881db7795e732aeab090ea55899a3883e
Ehbebe <study2hojin@gmail.com>
2024-04-20 23:57:41 +0900

    #6 Models And Admin Part01

---
+++ rooms/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+
+
+class Room(models.Model):
+    """숙소 모델 정의"""
+
+    class RoomKindChoices(models.TextChoices):
+        """숙소 종류에 대한 선택 옵션을 정의하는 내부 클래스"""
+
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = "shared_room", "Shared Room"
+
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+        verbose_name="국가",
+        help_text="숙소가 위치한 국가를 입력하세요.",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+        verbose_name="도시",
+        help_text="숙소가 위치한 도시를 입력하세요.",
+    )
+    price = models.PositiveIntegerField(
+        verbose_name="가격",
+        help_text="숙소의 1박 기준 가격을 입력하세요.",
+    )
+    rooms = models.PositiveIntegerField(
+        verbose_name="방 개수",
+        help_text="숙소의 방 개수를 입력하세요.",
+    )
+    toilets = models.PositiveIntegerField(
+        verbose_name="화장실 개수",
+        help_text="숙소의 화장실 개수를 입력하세요.",
+    )
+    description = models.TextField(
+        verbose_name="숙소 설명",
+        help_text="숙소에 대한 상세한 설명을 입력하세요.",
+    )
+    address = models.CharField(
+        max_length=250,
+        verbose_name="상세 주소",
+        help_text="숙소의 상세 주소를 입력하세요.",
+    )
+    pet_friendly = models.BooleanField(
+        default=True,
+        verbose_name="반려동물 동반 가능 여부",
+        help_text="반려동물 동반 가능 여부를 선택하세요.",
+    )
+    kind = models.CharField(
+        max_length=20,
+        choices=RoomKindChoices.choices,
+        verbose_name="숙소 종류",
+        help_text="숙소의 종류를 선택하세요.",
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        verbose_name="숙소 주인",
+        help_text="숙소의 주인을 선택하세요.",
+    )
+
+
+class Amenity(models.Model):
+    """숙소 편의시설 모델 정의"""
+
+    name = models.CharField(
+        max_length=150,
+        verbose_name="편의시설 이름",
+        help_text="편의시설의 이름을 입력하세요.",
+    )
+    description = models.CharField(
+        max_length=150,
+        null=True,
+        verbose_name="편의시설 설명",
+        help_text="편의시설에 대한 설명을 입력하세요. (선택 사항)",
+    )
--- users/models.py
+++ users/models.py
@@ -2,12 +2,42 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
 class User(AbstractUser):
-    # 이름과 성을 수정하지 못하도록 설정
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
 
-    # 한국식으로 이름을 작성할 수 있도록 설정
-    name = models.CharField(max_length=150, default="")
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.ImageField(blank=True)
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
     is_host = models.BooleanField(default=False)
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
